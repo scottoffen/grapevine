6c12826df4f4236647c136e27d317ab69cfa2225
Scott Offen <3513626+scottoffen@users.noreply.github.com>
2021-01-05 19:28:48 -0700

    Deploy website - based on 31ab21c8fc7a5f29ff6e3aaadb027c7f4f6fe2df

--- 323e1239.250b9f94.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{77:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Grapevine 5.0 Beta 1","permalink":"/grapevine/blog/beta-1"},{"title":"Grapevine Docs","permalink":"/grapevine/blog/docusaurus"},{"title":"Grapevine 5.0 Beta 0","permalink":"/grapevine/blog/beta-0"}]}')}}]);
