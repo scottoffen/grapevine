5751fdcd2ad495700adc146a176a67dd6efecb97
Scott Offen <3513626+scottoffen@users.noreply.github.com>
2021-01-07 23:00:57 -0700

    Adds extension methods to enable and disable routes

--- src/Grapevine/IRoute.cs
+++ src/Grapevine/IRoute.cs
@@ -13,4 +13,19 @@ namespace Grapevine
 
         IRoute WithHeader(string header, Regex pattern);
     }
+
+    public static class IRouteExtensions
+    {
+        public static IRoute Disable(this IRoute route)
+        {
+            route.Enabled = false;
+            return route;
+        }
+
+        public static IRoute Enable(this IRoute route)
+        {
+            route.Enabled = true;
+            return route;
+        }
+    }
 }
