7cae4d98cfff66062c3942eaccfaa2bf44e69468
Scott Offen <3513626+scottoffen@users.noreply.github.com>
2022-03-03 23:11:02 -0700

    Add check for entity body in form parsing middleware

--- src/Grapevine/Middleware/FormUrlEncodedData.cs
+++ src/Grapevine/Middleware/FormUrlEncodedData.cs
@@ -6,7 +6,7 @@ namespace Grapevine.Middleware
     {
         public async static Task Parse(IHttpContext context, IRestServer server)
         {
-            if (context.Request.ContentType != "application/x-www-form-urlencoded") return;
+            if ((context.Request.ContentType != ContentType.FormUrlEncoded) || (!context.Request.HasEntityBody)) return;
             context.Locals.TryAdd("FormData", await context.Request.ParseFormUrlEncodedData());
         }
     }
