b3e35b89236e58258101702aa3a18a02c9b1899d
SpReeDateD <webmaster@born2skate.de>
2022-06-25 13:50:56 +0200

    strong signed assembly

---
+++ src/Grapeseed/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+// In SDK-style projects such as this one, several assembly attributes that were historically
+// defined in this file are now automatically added during build and populated with
+// values defined in project properties. For details of which attributes are included
+// and how to customise this process see: https://aka.ms/assembly-info-properties
+
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM
+// components.  If you need to access a type in this assembly from COM, set the ComVisible
+// attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+
+[assembly: Guid("d527a55d-7447-430a-893f-ce6341b27aeb")]
+[assembly: InternalsVisibleTo("Grapevine.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010065d38f9f40af2e7e09cfaf8146ef706da34037cb50e1bf47d456162b5f4512c12ea4743165dc1d01c7565800ce8591c8094ec5ab312a95e980756ad83ab4f66f8f109f309e526183b1a9d6e1f6edb01cc5a10ec2784c31aa680e4217d2559e3f19060932b08957214f32efdd0e561b4231d33ee1803b6f5606750c40f9cd6ed4")]
